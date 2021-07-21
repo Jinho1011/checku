b047fd0d26199bdaceec4dc357e53be088696a15
jinho1011 <gino9940@gmail.com>
2021-07-22 00:36:38 +0900

    fix order

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
-import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import Stack from './navigation/Stack';
 
