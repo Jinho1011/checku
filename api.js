6c5cd36d0a80a08896fc843c9ea21b595401cb58
jinho1011 <gino9940@gmail.com>
2021-07-23 01:32:22 +0900

    fix url

--- api.js
+++ api.js
@@ -1,11 +1,24 @@
 export const login = (id, pwd) => {
-  return fetch('http://api.checku.site/login', {
+  return fetch('http://3.38.19.120/login', {
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
     body: JSON.stringify({id: id, pwd: pwd}),
   })
     .then(response => {
       return response.text();
+    })
+    .catch(error => {
+      throw error;
+    });
+};
+
+export const onload = () => {
+  return fetch('http://3.38.19.120/onload', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+  })
+    .then(response => {
+      return response;
     })
     .catch(error => {
       throw error;
