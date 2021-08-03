0dd8f1f09ec6551e4b1f7e5908147b998ae520b2
jinho1011 <gino9940@gmail.com>
2021-08-04 00:23:52 +0900

    add tab icons

--- navigation/Tab.js
+++ navigation/Tab.js
@@ -1,5 +1,8 @@
 import * as React from 'react';
+import {Text} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 import GradeContainer from '../screens/Grade/GradeContainer';
 import GraduateContainer from '../screens/Graduate/GraduateContainer';
@@ -9,7 +12,59 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+          let tabName;
+          let iconColor = focused ? '#00CA64' : '#F1F6F4';
+          let iconSize = 24;
+
+          const Icon = () => {
+            if (route.name === 'grade') {
+              iconName = 'alphabetical-variant'; // MaterialCommunityIcons
+              tabName = '성적';
+              return (
+                <MaterialCommunityIcons
+                  name={iconName}
+                  size={iconSize}
+                  color={iconColor}
+                />
+              );
+            } else if (route.name === 'graduate') {
+              iconName = 'graduation-cap'; // FontAwesome5
+              tabName = '졸업';
+              return (
+                <FontAwesome5
+                  name={iconName}
+                  size={iconSize}
+                  color={iconColor}
+                />
+              );
+            } else if (route.name === 'course') {
+              iconName = 'book'; // MaterialCommunityIcons
+              tabName = '수강신청';
+              return (
+                <MaterialCommunityIcons
+                  name={iconName}
+                  size={iconSize}
+                  color={iconColor}
+                />
+              );
+            }
+          };
+
+          return (
+            <>
+              <Icon />
+              <Text>{tabName}</Text>
+            </>
+          );
+        },
+      })}
+      tabBarOptions={{
+        showLabel: false,
+      }}>
       <Tab.Screen name="grade" component={GradeContainer} />
       <Tab.Screen name="graduate" component={GraduateContainer} />
       <Tab.Screen name="course" component={CourseContainer} />
