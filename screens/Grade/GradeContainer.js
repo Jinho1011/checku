a9339d55db0d826f913a5a0bd0a871cba79fbc57
jinho1011 <gino9940@gmail.com>
2021-08-04 00:23:47 +0900

    check valid json form

--- screens/Grade/GradeContainer.js
+++ screens/Grade/GradeContainer.js
@@ -5,6 +5,15 @@ import {gradeAll} from '../../api';
 import {getData, storeData} from '../../storage';
 import GradePresenter from './GradePresenter';
 
+function IsJsonString(str) {
+  try {
+    var json = JSON.parse(str);
+    return typeof json === 'object';
+  } catch (e) {
+    return false;
+  }
+}
+
 export default () => {
   const [grades, setGrades] = useState({});
 
@@ -38,6 +47,7 @@ export default () => {
         gradeObj['AVG'] = GRADE;
       }
     });
+
     setGrades(gradeObj);
     storeData('@grades', gradeObj);
   };
@@ -46,11 +56,15 @@ export default () => {
     const init = async () => {
       try {
         let stdNo = await getData('@stdNo');
-        let GRADES = await gradeAll(stdNo);
-        GRADES = JSON.parse(GRADES);
         let storedGrades = await getData('@grades');
-
         if (storedGrades != null) setGrades(storedGrades);
+
+        let GRADES = await gradeAll(stdNo);
+        try {
+          GRADES = JSON.parse(GRADES);
+        } catch (e) {
+          console.error(e);
+        }
         initGrades(GRADES);
       } catch (error) {
         throw error;
